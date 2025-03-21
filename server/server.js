ecfce412765ce82ad63a64a3edc5873d43f33d53
Noa Gilboa <nnooaa5@gmail.com>
2025-03-21 12:12:46 +0200

    Fix startup command and port

--- server/server.js
+++ server/server.js
@@ -9,7 +9,10 @@ require('dotenv').config();
 
 
 const app = express();
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Server is running on port ${PORT}`);
+});
 
 // Middleware
 app.use(cors({
