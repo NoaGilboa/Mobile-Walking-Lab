1a8cdc8759fe531fec6461b59ae8bdacb0053dc8
Noa Gilboa <nnooaa5@gmail.com>
2025-05-17 20:34:05 +0300

    registerTherapist

--- web_interface/src/api/therapistApi.js
+++ web_interface/src/api/therapistApi.js
@@ -2,8 +2,8 @@ import axios from 'axios';
 import { BASE_URL } from './apiConfig';
 
 // Function to register a therapist
-export const registerTherapist = (therapistId, email, password, name) => {
-  return axios.post(`${BASE_URL}/therapists/register`, {therapistId, email, password,name });
+export const registerTherapist = (therapist_id, email, password, name) => {
+  return axios.post(`${BASE_URL}/therapists/register`, {therapist_id, email, password,name });
 };
 
 // Function to login a therapist
