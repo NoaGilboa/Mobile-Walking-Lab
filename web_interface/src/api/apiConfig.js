7df490a024c6cbc7cea7758e51946917342b7bff
Noa Gilboa <nnooaa5@gmail.com>
2025-06-26 23:06:33 +0300

    ESP32_BASE_URL

--- web_interface/src/api/apiConfig.js
+++ web_interface/src/api/apiConfig.js
@@ -1,3 +1,3 @@
 export const BASE_URL = "https://walkinglab-hbesf8g3aaa8hafz.westeurope-01.azurewebsites.net/api";
-export const ESP32_BASE_URL = "https://192.168.1.92";
+export const ESP32_BASE_URL = "http://192.168.1.92";
 
