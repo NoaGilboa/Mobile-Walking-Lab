64af0197a8921bc05f1d5ef61c706aa45580f8c7
Noa Gilboa <nnooaa5@gmail.com>
2025-06-28 14:26:31 +0300

    startESP32Session

--- web_interface/src/api/deviceApi.js
+++ web_interface/src/api/deviceApi.js
@@ -1,17 +1,18 @@
 import axios from 'axios';
 import { ESP32_BASE_URL } from './apiConfig';
+import { BASE_URL } from './apiConfig';
 
 // התחלת מדידה בבקר
 export const startESP32Session = () => {
-  return axios.get(`${ESP32_BASE_URL}/start-session`);
+  return axios.get(`${BASE_URL}/device/start-session`);
 };
 
 // עצירת מדידה בבקר
 export const stopESP32Session = () => {
-  return axios.get(`${ESP32_BASE_URL}/stop-session`);
+  return axios.get(`${BASE_URL}/stop-session`);
 };
 
 // בדיקת מצב הבקר
 export const getESP32Status = () => {
-  return axios.get(`${ESP32_BASE_URL}/status`);
+  return axios.get(`${BASE_URL}/status`);
 };
